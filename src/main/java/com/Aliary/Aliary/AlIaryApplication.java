b640511655ec3328f90a26b90de617e0e1ae7928
Mootata <dbslt6872@gmail.com>
2022-06-06 23:38:28 +0900

    git init

---
+++ src/main/java/com/Aliary/Aliary/AlIaryApplication.java
@@ -0,0 +1,13 @@
+package com.Aliary.Aliary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlIaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlIaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Aliary/Aliary/AlIaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aliary.Aliary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlIaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
