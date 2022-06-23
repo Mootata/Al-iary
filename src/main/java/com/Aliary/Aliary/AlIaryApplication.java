61237eacdb2b2be5a6a56e038e2c8356c7097331
Mootata <dbslt6872@gmail.com>
2022-06-24 02:21:15 +0900

    설정 파일 생성

--- src/main/java/com/Aliary/Aliary/AlIaryApplication.java
+++ src/main/java/com/Aliary/Aliary/AlIaryApplication.java
@@ -2,12 +2,16 @@ package com.Aliary.Aliary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AlIaryApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(AlIaryApplication.class, args);
+		new SpringApplicationBuilder(AlIaryApplication.class).properties(APPLICATION_LOCATIONS).run(args);
 	}
 
 }
